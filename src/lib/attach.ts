617dd74029e36a8479aac104bc241d89ba48ba97
jill64 <143883742+jill64@users.noreply.github.com>
2023-11-10 20:21:39 +0900

    Update attach.ts

--- src/lib/attach.ts
+++ src/lib/attach.ts
@@ -1,11 +1,11 @@
-import { transform } from '@jill64/svelte-html'
+import { apply } from '@jill64/svelte-html'
 import type { Handle } from '@sveltejs/kit'
 import { get } from 'svelte/store'
 import { store } from './store'
 
 export const attach = (({ event, resolve }) =>
   resolve(event, {
-    transformPageChunk: transform({
+    transformPageChunk: apply({
       prefix: get(store).prefix
     })
   })) satisfies Handle
