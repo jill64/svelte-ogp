f70a3e151491b67d9c7aa44e863e1faf0483c174
jill64 <intents.turrets0h@icloud.com>
2023-11-11 10:54:23 +0900

    Add Sentry error handling to server hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,8 @@
-export { attach as handle } from './lib/attach'
+import { PUBLIC_SENTRY_DSN } from '$env/static/public'
+import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
+import { attach } from './lib/attach'
+
+const { onHandle, onError } = serverInit(PUBLIC_SENTRY_DSN)
+
+export const handle = onHandle(attach)
+export const handleError = onError()
