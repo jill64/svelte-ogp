5d25fd9f48e3ce7a19cc42c5beaa7384a7c7d628
jill64 <intents.turrets0h@icloud.com>
2023-12-12 19:02:57 +0900

    chore: refactor with npm-demo-layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>@jill64/svelte-ogp</h1>
+<script lang="ts">
+  import { page } from '$app/stores'
+  import { Highlight, HighlightSvelte } from '@jill64/npm-demo-layout/highlight'
+  import { javascript, xml } from '@jill64/npm-demo-layout/highlight/languages'
+  import { code } from './code'
+
+  $: sample = code($page.url)
+</script>
+
 Open DevTools and make sure OGP is set.
+
+<main>
+  <div>
+    <Highlight language={javascript} code={sample.hook} />
+    <HighlightSvelte code={sample.svelte} />
+  </div>
+  <section>
+    <h2>Converted HTML</h2>
+    <Highlight language={xml} code={sample.html} />
+  </section>
+</main>
+
+<style>
+  main {
+    display: grid;
+    grid-template-columns: auto auto;
+    column-gap: 2rem;
+  }
+  @media (max-width: 1200px) {
+    main {
+      grid-template-columns: auto;
+    }
+  }
+  div,
+  section {
+    overflow-x: auto;
+  }
+</style>
---
+++ src/routes/code.ts
@@ -0,0 +1,38 @@
+import { description, name } from '../../package.json'
+
+export const code = (url: URL) => ({
+  svelte: /* html */ `
+<!-- +layout.svelte -->
+<script>
+  import { OGP } from '@jill64/svelte-ogp'
+</script>
+
+<OGP
+  title="${name}"
+  description="${description}"
+  site_name="${name}"
+  image="/og-image.png"
+/>
+`.trim(),
+
+  hook: `
+// hooks.server.js
+export { attach as handle } from '@jill64/svelte-ogp'
+`.trim(),
+
+  html: /* html */ `
+<html prefix="og: https://ogp.me/ns#">
+  <head>
+    <meta property="og:title" content="${name}">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="${url.origin}/og-image.ong">
+    <meta property="og:url" content="${url.href}">
+    <meta property="og:description" content="${description}">
+    <meta property="og:site_name" content="${name}">
+    <meta name="twitter:card" content="summary_large_image">
+    <!-- ... -->
+  </head>
+  <!-- ... -->
+</html>
+`.trim()
+})
